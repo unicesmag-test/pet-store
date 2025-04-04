365f7020c3b564496eb5a310e8ad86cf3cdaff51
Joan <joan@gmail.com>
2025-04-03 20:02:50 -0500

    Validate if email exists in signup request

--- src/signup.php
+++ src/signup.php
@@ -1,5 +1,5 @@
 <?php
-
+    
     include('config/database.php');
     
     $fname = $_POST['f_name'];
@@ -7,14 +7,34 @@
     $email = $_POST['e_mail'];
     $passw = $_POST['p_assw'];
 
-    $sql = "INSERT INTO users
-            (firstname, lastname, email, password)
-            VALUES ('$fname','$lname','$email','$passw')
+    $sql_validate_email = "
+        select 
+            count(id) as total 
+        from
+            users
+        where
+            email = '$email' and
+            status = true    
     ";
-    $ans = pg_query($conn, $sql);
-    if($ans){
-        echo "User has been created successfully";
+    $ans = pg_query($conn, $sql_validate_email);
+
+    if($ans){ 
+        $row = pg_fetch_assoc($ans);
+        if($row['total'] > 0){
+            echo "User already exits !!!";
+        }else{
+            $sql = "INSERT INTO users
+                    (firstname, lastname, email, password)
+                    VALUES ('$fname','$lname','$email','$passw')
+            ";
+            $ans = pg_query($conn, $sql);
+            if($ans){
+                echo "User has been created successfully";
+            }else{
+                echo "Error";
+            }
+        }
     }else{
-        echo "Error";
+        echo "Query error";
     }
 ?>
